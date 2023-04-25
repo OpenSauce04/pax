e0fc5889089476f771b7d2fb23bc94c561a0bc44
OpenSauce04 <opensauce04@gmail.com>
2023-04-25 18:27:37 +0100

    Nix package now uses Ruby 3.2

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@ let
 in stdenv.mkDerivation {
   name = "pax-apx";
   src = ./.;
-  buildInputs = [ruby];
+  buildInputs = [ruby_3_2];
   installPhase = ''
     mkdir -p $out/bin
     cp $src/pax $out/bin
